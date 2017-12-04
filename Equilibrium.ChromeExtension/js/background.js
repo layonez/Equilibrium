2d280f2d64737ddca323c76c55c2397df248f15e
layonez <layonez@live.com>
2017-12-04 20:11:36 +0300

    Обновление всплывающего окошка и текстов нотификации

--- Equilibrium.ChromeExtension/js/background.js
+++ Equilibrium.ChromeExtension/js/background.js
@@ -29,23 +29,48 @@ chrome.browserAction.setTitle({
 
 var icons = ['images/red_48.png', 'images/yellow_48.png', 'images/green_48.png'];
 function resetIcon(disbalance) {
-
+	//i.Зеленый – выводится подсказка  « <дисбаланс по правилам расчета для блокировок> - у вас все списано»
+	//ii.      Желтый – выводится подсказка  «<дисбаланс по правилам расчета для блокировок>  - у вас будет заблокирован интернет через 1(-3) дня, спишите время»
+	//iii.      Красный –  выводится подсказка  « <дисбаланс по правилам расчета для блокировок>  - у вас заблокирован интернет, спишите время»
     if (disbalance < -50) {
 	    chrome.browserAction.setIcon({ path: icons[0] });
 
         chrome.browserAction.setBadgeText({ text: (-disbalance) + "ч"});
 			chrome.browserAction.setTitle({
-                title:'Текущий дисбаланс составляет '+ (-disbalance) +'ч. Интернет отключен за неуплату.'
+                title: 'Текущий дисбаланс составляет ' + (-disbalance) +'ч. У вас заблокирован интернет, спишите время.'
     });
-    } else if (-50 < disbalance < -30) {
+    } else if (-50 <= disbalance <= -10) {
 	    chrome.browserAction.setIcon({ path: icons[1] });
 
-	    chrome.browserAction.setBadgeText({ text: (-disbalance) + "ч" });
-            chrome.browserAction.setTitle({ title: 'Текущий дисбаланс составляет ' + (-disbalance) + 'ч. Интернет будет отключен за неуплату через ' + (5 - Math.floor((-disbalance)/10)) +' дня.'});
+        chrome.browserAction.setBadgeText({ text: (-disbalance) + "ч" });
+        const daysToBlockInternet = (5 - Math.floor((-disbalance) / 10));
+        
+        chrome.browserAction.setTitle({ title: 'Текущий дисбаланс составляет ' + (-disbalance) + 'ч. Интернет будет заблокирован ' + getDayWord(daysToBlockInternet)});
 
     } else {
 	    chrome.browserAction.setBadgeText({ text: (-disbalance) + "ч" });
-	    chrome.browserAction.setIcon({ path: icons[2] });
-	    chrome.browserAction.setTitle({ title: 'Текущий дисбаланс составляет ' + (-disbalance) + 'ч. Интернет будет отключен за неуплату через ' + (5 - Math.floor((-disbalance) / 10)) + ' дней.' });
+        chrome.browserAction.setIcon({ path: icons[2] });
+
+        const daysToBlockInternet = (5 - Math.floor((-disbalance) / 10));
+        chrome.browserAction.setTitle({ title: 'Текущий дисбаланс составляет ' + (-disbalance) + 'ч. Интернет будет заблокирован ' + getDayWord(daysToBlockInternet)});
 	}
 };
+
+function getDayWord(daysToBlockInternet) {
+	let dayWord;
+    switch (daysToBlockInternet) {
+    case 0:
+	        dayWord = ' сегодня';
+	    break;
+	case 1:
+            dayWord = `через ${daysToBlockInternet} день`;
+		break;
+	case 5:
+            dayWord = `через ${daysToBlockInternet} дней`;
+        break;
+	default:
+            dayWord = `через ${daysToBlockInternet} дня`;
+		break;
+    }
+	return dayWord;
+}
